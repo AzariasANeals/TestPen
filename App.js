404b341c10c943f1664d06d1f1f5a309bc4fbb46
AzariasANeals <Azarias.P.Aneals@seattlecolleges.edu>
2024-04-23 23:39:31 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const applicants = [{
+  name: 'Joe', work: 'freelance-developer',
+  blogs: '54', websites:'32',
+  hackathons: '6', location: 'morocco', id:'0'
+},
+  {
+    name:'Janet', work:'fullstack-developer',
+    blogs:'34', websites:'32',
+    hackathons:'8', location:'Mozambique', id:'1',
+  }
+];
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View styles={styles.container}>
+    <>
+    {applicants.map(function(applicant) {
+      return (
+        <Text key={applicant.id} style={styles.textFont}>
+          <Text style={styles.p}>Applicant Name:{applicant.name}</Text>
+
+          <Text>Applicant location:{applicant.location}</Text>
+          
+          <Text>Hackathons participated:{applicant.hackathons}</Text>
+
+        </Text>
+      )
+    })}
+    </>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +42,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  p: {
+    marginTop: 5,
+    marginBottom:5,
+    paddingVertical:20,
+    lineHeight:50
+  },
+  textFont:{
+    marginTop:100,
+    marginLeft:50,
+    marginRight:50,
+    marginBottom:10,
+    fontSize:20,
+    fontWeight:'100',
+  }
 });
